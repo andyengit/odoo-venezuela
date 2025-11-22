b5fff5fe9baba3948bc88d239f655da5ad03a69d
Andy <anderson.armeya@gmail.com>
2025-11-21 21:20:44 -0400

    [18.0][MIG] l10n_ve_base
    Migracion de l10n_ve_base a 18.0

---
+++ l10n_ve_base/static/src/core/debug/debug_menu_items.esm.js
@@ -0,0 +1,9 @@
+/** @odoo-module **/
+
+import {registry} from "@web/core/registry";
+import {becomeSuperuser} from "@web/core/debug/debug_menu_items";
+
+registry
+    .category("debug")
+    .category("default")
+    .remove("becomeSuperuser", becomeSuperuser);
