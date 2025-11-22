9bb0dd6f3f0692ca146f0f529ba8ea4d292d6233
Andyclodofy <andy@clodofy.com>
2025-11-22 01:26:36 +0000

    [IMP] l10n_ve_rate: pre-commit execution

---
+++ l10n_ve_rate/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Venezuela - Tipo de cambio",
+    "summary": """
+        Módulo para configurar el tipo de cambio de Venezuela
+    """,
+    "license": "LGPL-3",
+    "author": "binaural-dev",
+    "website": "https://binauraldev.com/",
+    "category": "Technical",
+    "version": "17.0.0.0.3",
+    # any module necessary for this one to work correctly
+    "depends": ["base", "l10n_ve_base"],
+    "data": [
+        "views/res_config_settings.xml",
+    ],
+}
